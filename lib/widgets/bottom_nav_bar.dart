0283ba9c4ce9b384a6c560c75a8e2404478de5e4
Mariz Yalung <yalungmariz@gmail.com>
2024-02-23 00:01:10 +0800

    Mariz Yalung

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meditation_app/constants.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      height: 80,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          BottomNavItem(
+            title: "Today",
+            svgScr: "assets/icons/calendar.svg",
+          ),
+          BottomNavItem(
+            title: "All Exercises",
+            svgScr: "assets/icons/gym.svg",
+            isActive: true, // Set isActive to true for the active item
+          ),
+          BottomNavItem(
+            title: "Settings",
+            svgScr: "assets/icons/Settings.svg",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String svgScr;
+  final String title;
+  final bool isActive; // Add isActive parameter
+  final void Function()? press;
+  const BottomNavItem({
+    Key? key,
+    required this.svgScr,
+    required this.title,
+    this.isActive = false, // Assign a default value
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SvgPicture.asset(
+            svgScr,
+            color: isActive ? kActiveIconColor : kTextColor,
+          ),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
